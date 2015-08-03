be56d4751d423813a65d9e772f6ee7602915b2e9
divsense <oleg4564@iOK.local>
2015-08-03 00:28:11 -0400

    remove added

---
+++ db/remove.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var argv = require('minimist')(process.argv.slice(2));
+var Datastore = require("nedb");
+
+var dbpath = argv.path;
+var nodeid = argv.node;
+
+if( !dbpath ){
+
+	process.stderr.write("Path parameter required");
+	process.exit(9);
+
+}
+
+if( !nodeid ){
+
+	process.stderr.write("Node parameter required");
+	process.exit(9);
+
+}
+
+process.stdin.setEncoding('utf8');
+
+var data = "";
+
+process.stdin.on("readable", function(){
+	var chunk = process.stdin.read();
+	if( chunk ){
+		data += chunk;
+	}
+});
+
+process.stdin.on("end", function(){
+
+	var json_data;
+
+	if( !data ){
+		process.stderr.write("Empty data");
+		process.exit(9);
+	}
+
+	try{
+		json_data = JSON.parse( data );
+	}
+	catch(e){
+		process.stderr.write("Invalid JSON data");
+		process.exit(9);
+	}
+
+	if( !Array.isArray( json_data ) ){
+		process.stderr.write("Array expected");
+		process.exit(9);
+	}
+
+	var db = new Datastore({filename: dbpath});
+
+	db.loadDatabase(function(err){
+
+		if( err ){
+			process.stderr.write(JSON.stringify(err));
+			process.exit(9);
+		}
+
+		db.remove({_id: nodeid}, {}, function(err){
+			if( err ){
+				process.stderr.write(JSON.stringify(err));
+				process.exit(9);
+			}
+		});
+	});
+});
